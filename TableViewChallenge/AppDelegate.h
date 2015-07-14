0f95fa464a8974cd5ee7780e726a864fbc5503e0
MarcoRmz <marcormz19@gmail.com>
2015-07-14 15:50:33 -0500

    initial commit

---
+++ TableViewChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewChallenge
+//
+//  Created by Marco Ramirez on 7/14/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewChallenge/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  TableViewChallenge
+//
+//  Created by Marco Ramirez on 7/14/15.
+//  Copyright (c) 2015 Marco Ramirez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
